000670d9ac0cc565b0b13ae8c401f5c81caf439a
Phil Weir <phil.weir@flaxandteal.co.uk>
2016-11-03 00:53:49 +0000

    add an index.js for webpack to find it

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports.Map = require('./src/components/Map.vue')
+module.exports.Tilelayer = require("./src/components/TileLayer.vue")
+module.exports.Marker = require("./src/components/Marker.vue")
+module.exports.Popup = require("./src/components/Popup.vue")
+
+module.exports.MapComponent = require('./src/components/mapComponent')
